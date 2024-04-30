bf4d5a50ec0a07024a24e7f3c70972bb8d9f7ba1
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-30 14:51:17 -0300

    feito em sala commit

--- 5by5-EstanteLivros/Program.cs
+++ 5by5-EstanteLivros/Program.cs
@@ -1,83 +1,109 @@
-﻿int qtd_livros;
-do
+﻿using EstanteLivro;
+
+//Livro l = new("Senhor dos Anéis", "J. R. R. Tolkien", DateOnly.Parse("03/11/2001"), "Saraiva", 1, "1234567", 500);
+
+Livro[] estante = new Livro[10];
+int indice = 0, op = 0;
+
+int Menu()
 {
-    Console.WriteLine("Quantos livros deseja adicionar a estante: ");
-    qtd_livros = int.Parse(Console.ReadLine());
-    if (qtd_livros < 1)
+    Console.WriteLine("1 - Cadastrar Livro");
+    Console.WriteLine("2 - Imprimir Estante");
+    Console.WriteLine("3 - Imprimir Livro Especifico");
+    Console.WriteLine("4 - Sair");
+    Console.WriteLine("Escolha sua opção: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+Livro CadastrarLivro()
+{
+    string titulo, autor, editora, isbn;
+    int edicao, paginas;
+    DateOnly lancamento;
+
+    Console.WriteLine("Informe o titulo do livro: ");
+    titulo = Console.ReadLine();
+    Console.WriteLine("Informe o autor do livro: ");
+    autor = Console.ReadLine();
+    Console.WriteLine("Informe a data de lançamento: ");
+    lancamento = DateOnly.Parse(Console.ReadLine());
+    Console.WriteLine("Informe a editora: ");
+    editora = Console.ReadLine();
+    Console.WriteLine("Informe a edição: ");
+    edicao = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o ISBN: ");
+    isbn = Console.ReadLine();
+    Console.WriteLine("Informe a quantidade de páginas: ");
+    paginas = int.Parse(Console.ReadLine());
+
+    return new(titulo, autor, lancamento, editora, edicao, isbn, paginas);
+
+}
+void AdicionarLivroNaEstante()
+{
+    do
     {
-        Console.WriteLine("A estante precisa ter pelo menos 1 livro");
-    }
-} while (qtd_livros <= 0);
-Livros [] estante = new Livros[qtd_livros];
-int op;
-Livros Criar_livro()
-{
-    Livros l = new Livros();
-    Console.WriteLine("Digite o Titulo do Livro: ");
-    l.DefinirTitulo(Console.ReadLine());
-    Console.WriteLine("Digite o autor do Livro: ");
-    l.DefinirAutor(Console.ReadLine());
-    Console.WriteLine("Digite a data de lançamento do Livro: ");
-    l.DefinirLancamento(DateOnly.Parse(Console.ReadLine()));
-    Console.WriteLine("Digite a editora do Livro: ");
-    l.DefinirEditora(Console.ReadLine());
-    Console.WriteLine("Digite o numero da edição do Livro: ");
-    l.DefinirEdicao(int.Parse(Console.ReadLine()));
-    Console.WriteLine("Digite o numero de isbn do Livro: ");
-    l.DefinirISBN(Console.ReadLine());
-    Console.WriteLine("Digite a quantidade de paginas do Livro: ");
-    l.DefinirPaginas(int.Parse(Console.ReadLine()));
-    return l;
+        Livro l = CadastrarLivro();
+        estante[indice] = l;
+        Console.WriteLine($"{indice + 1}/10 livros na estante");
+        indice++;
+        Console.WriteLine("Deseja cadastrar um novo livro?\n[1]SIM\n[2]NÃO");
+        op = int.Parse(Console.ReadLine());
+    } while (op != 0 && indice < 10);
 }
-
-for (int i = 0; i < estante.Length; i++)
+void ImprimirEstante()
 {
-    estante[i] = Criar_livro();
+    for (int i = 0; i < indice; i++)
+        estante[i].ImprimirLivro();
+}
+void MostrarTitulos()
+{
+    for (int i = 0; i < indice; i++)
+    {
+        Console.Write($"{i + 1}° Livro -> ");
+        estante[i].ImprimiLivros();
+
+    }
+}
+void BuscarLivro(int i)
+{
+    while (i >= indice)
+    {
+        Console.WriteLine("O indice não existe na estante, digite um novo: ");
+        i = int.Parse(Console.ReadLine());
+    }
+    estante[i].ImprimirLivro();
 }
 
 do
 {
-    Console.WriteLine("============= OPÇÕES =============");
-    Console.WriteLine("[1] - Mostrar Livros\n[2] - Mostrar Detalhes Livro\n[3] - Mostrar Todos os Livros\n[4] - SAIR");
-    op = int.Parse(Console.ReadLine());
-    switch (op)
+    switch (Menu())
     {
         case 1:
-            Console.WriteLine("=============== LIVROS ==================");
-            for (int i = 0; i < estante.Length; i++)
-            {
-                Console.WriteLine($"[{i}]" + estante[i].titulo);
-            }
+            AdicionarLivroNaEstante();
             break;
         case 2:
-            Console.WriteLine("Digite o indice do livro na estante: ");
-            int indice_livro = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Livro {indice_livro}");
-            Console.WriteLine("========================");
-            estante[indice_livro].ImprimirLivro();
-            Console.WriteLine("========================");
+            ImprimirEstante();
             break;
         case 3:
-            for (int i = 0; i < estante.Length; i++)
-            {
-                Console.WriteLine($"Livro {i}");
-                Console.WriteLine("========================");
-                estante[i].ImprimirLivro();
-                Console.WriteLine("========================");
-            }
-                break;
-        case 4:
+            Console.WriteLine("==================== TITULOS NA ESTANTE ====================");
+            MostrarTitulos();
+            Console.Write("Informe o indice do livro: ");
+            BuscarLivro(int.Parse(Console.ReadLine()) - 1);
             break;
-
+        case 4:
+            Console.WriteLine("Hasta la vista, baby!");
+            Environment.Exit(0);
+            break;
         default:
-            Console.WriteLine("Numero invalido");
+            Console.WriteLine("Opção Inválida!");
             break;
     }
-
-    
-} while (op != 4);
+} while (true);
 
 
 
 
 
+Console.ReadLine();
